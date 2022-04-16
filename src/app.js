ff5bfe3ff8df4e6a58c6054ec8bd5e1a8fcec52e
Paul Beard <pbeardey@gmail.com>
2022-04-16 17:14:50 +0100

    refactor to allow hello then match variable

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/strings/hello/:greet', function(req, res) {
+  const greeting = req.params.greet;
+  res.send({ result: `Hello, ${greeting}!` });
+});
+
 module.exports = app;
